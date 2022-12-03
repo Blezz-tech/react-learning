97b047a9f81778c3f6c0df67fc6223b9a7537b30
Blezz-tech <markus.jenya04@gmail.com>
2022-12-03 16:58:19 +0300

    vv

--- src/App.js
+++ src/App.js
@@ -7,28 +7,36 @@ import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePost";
 import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./utils/pages";
 
 function App() {
 
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  let pagesArray = getPagesArray(totalPages);
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPageCount(totalCount, limit));
+  });
 
   useEffect(() => {
     fetchPosts();
   }, []);
 
-
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
-
-  async function fetchPosts() {
-    const posts = await PostService.getAll();
-    setPosts(posts);
-  }
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -42,7 +50,18 @@ function App() {
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список" />
+      {postError &&
+        <h1>Произошла ошибка ${postError}</h1>
+      }
+      {isPostsLoading
+        ? <div style={{display:'flex', justifyContent: 'center', marginTop: "50px"}}><Loader /></div>
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список" />
+      }
+      <div style={{marginTop: "30px"}}>
+        {pagesArray.map(p => 
+          <MyButton>{p}</MyButton>
+        )}
+      </div>
     </div>
   );
 }
@@ -50,9 +69,9 @@ function App() {
 export default App;
 
 /*
-https://youtu.be/GNrdg3PzpJQ?t=6286
+https://www.youtube.com/watch?v=GNrdg3PzpJQ&t=5424s
 
-1:44:46
+2:02:30
 
 Смотри с этапа:
 
